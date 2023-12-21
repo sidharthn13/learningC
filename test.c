b5afe8bf065b7df2275dda8415876cbb817845d7
sidharthn13 <sidharth4network@gmail.com>
2023-12-21 18:26:00 +0530

    learned datatypes and casting

--- test.c
+++ test.c
@@ -1,12 +1,33 @@
-#include <stdio.h>
- int main(){
-    int var = 100;
-    int *pointer = & var;
-    *pointer = 99;
-    printf("changed value: %d\n",var);
-    printf("memory address : %p/n ", &var);
-    return 0;
- }
- 
+// #include <stdio.h>
+//  int main(){
+//     int var = 100;
+//     int *pointer = & var;
+//     *pointer = 99;
+//     printf("changed value: %d\n",var);
+//     printf("memory address : %p\n ", &var);
+//     printf("the dereferenced pointer is: %d\n" ,*pointer);
+//     return 0;
+//  }
+
+// #include <stdio.h>
+// void main(){
+//    int a = 10;
+//    int b = 4;
+//    double out = (double)a/ (double)b;  
+//    printf("the output is : %f\n",out);
+// }
+
+// #include <stdio.h>
+// void main(){
+//    char myChar = 'a';
+//    int myChar2 = (int)myChar;
+//    int *ptr = &myChar2;
+//    printf("the ascii value of the char datatype is: %d\n", *ptr); 
+//    }
+   
+
+
+
+
 
 
