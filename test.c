740077075f4c6ad7f7362b2ffecb1b27654ed2bb
sidharthn13 <sidharth4network@gmail.com>
2023-12-28 11:49:28 +0530

    program to traverse digits in a number

--- test.c
+++ test.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-void swap(int *p1, int *p2){
-    *p1 +=  *p2; 
-    *p2 = *p1 - *p2;
-    *p1 -= *p2;
-}
-void main(){
-    int num1;
-    int num2;
-    printf("enter first number:\n");
-    scanf("%d",&num1);
-    printf("enter second number:\n");
-    scanf("%d",&num2);
-    swap(&num1,&num2);
-    printf("%d\n%d\n",num1,num2);   
-}
-
+#include <math.h>
+void main()
+{
+    int num;
+    printf("enter the number:");
+    scanf("%d", &num);
+    int counter = 1;
+    while(num>=pow(10, counter)){
+        int digitSelector = round(pow(10,counter));
+        int digitsPlace = round(pow(10,counter-1));
+        if((num%digitSelector)/digitsPlace == 0){
+            num += digitsPlace;
+        }
+     counter++;
+    }
+    printf("The modified number is: %d\n", num);
+}  
