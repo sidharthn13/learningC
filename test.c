ec2e6d6415b3eda30becd20749a0460c593e7205
sidharthn13 <sidharth4network@gmail.com>
2023-12-30 14:39:57 +0530

    program to sort first half in ascending order,second half in descending order - using pointers

--- test.c
+++ test.c
@@ -1,18 +1,54 @@
 #include <stdio.h>
-void main(){
-    int shift = 2;
-    int arr[] = {1,2,3,4,5};
-    int size = sizeof(arr)/sizeof(int);
-    for(int i = 0; i< shift; i++){
-        int j = 0;
-        while(j< size -1){
-            arr[j]+= arr[j+1];
-            arr[j+1] = arr[j]- arr[j+1];
-            arr[j] -= arr[j+1];
-            j++;
+void swap(int *p1, int *p2)
+{
+    *p1 += *p2;
+    *p2 = *p1 - *p2;
+    *p1 -= *p2;
+}
+void bubbleSortAsc(int *arr, int size)
+{
+    while (size > 0)
+    {
+        int *p1 = arr;
+        int *p2 = p1 + 1;
+        while (p1 < arr + size)
+        {
+            if (*p1 > *p2)
+            {
+                swap(p1, p2);
+            }
+            p1++;
+            p2++;
         }
+        size--;
     }
-    for( int i = 0 ; i< size; i++){
-        printf("%d ", *(arr+i));
+}
+void bubbleSortDesc(int *arr, int size, int *endP)
+{
+    while (size > size / 2)
+    {
+        int *p1 = arr;
+        int *p2 = p1 + 1;
+        while (p1 < endP)
+        {
+            if (*p1 < *p2)
+            {
+                swap(p1, p2);
+            }
+            p1++;
+            p2++;
+        }
+        size--;
+    }
+}
+void main()
+{
+    int arr[] = { 11, 23, 42, 16, 83, 73, 59 };
+    int size = sizeof(arr) / sizeof(int);
+    bubbleSortAsc(arr, (size + 1) / 2);
+    bubbleSortDesc(arr + (size) / 2, size - 1, arr + size - 1);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d,", *(arr + i));
     }
 }
