46c06d6092a9bfac40d67b7234808caadfdc3617
sidharthn13 <sidharth4network@gmail.com>
2024-01-04 17:24:22 +0530

    learning structures

--- test.c
+++ test.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include<string.h>
 
-void generateSum(int arr[], int sIndex, int eIndex , int sum){
-    if( sIndex>eIndex){
-        printf("%d,",sum);
-        return;
-    }
-    generateSum(arr, sIndex+1, eIndex, sum+arr[sIndex]);
-    generateSum(arr, sIndex+1, eIndex, sum);
+struct Dog{
+    char breed[12];
+    int age;
+};
+
+int main(){
+    struct Dog dog1;
+    dog1.age = 2;
+    strcpy(dog1.breed, "alsatian ");    
+    printf("%s\n", dog1.breed);
+    struct Dog *structP = &dog1;
+    printf("the memory location of the struct is: %p\n", structP);
+    printf("the memory location of the breed member is: %p\n", structP);
+    printf("the memory location of the age member is: %p\n", structP+1);
+    return 0;
 }
-
-void main(){
-    int arr[] = {1,2,3};
-    int size = sizeof(arr)/sizeof(int);
-    generateSum(arr, 0, size-1 , 0);
-}   
