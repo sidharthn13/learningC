b7c3166a770c847b533bb691e3229ada5687d3ca
sidharthn13 <sidharth4network@gmail.com>
2024-01-17 22:28:48 +0530

    initialized dynamic array for generating all valid parantheses

--- test.c
+++ test.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 void generate(char arr[], int open, int close, int count)
 {
 
     if (open == 0 && close == 0)
     { 
+        *(arr+count) = '\0';
         printf("%s\n", arr);
         return;
     }
     if (open > 0)
     { 
-        arr[count] = '(';
+        *(arr+count) = '(';
         generate(arr, open - 1, close, count + 1);
     }
 
     if (open < close)
     { 
-        arr[count] = ')';
+        *(arr+count) = ')';
         generate(arr, open, close - 1, count + 1);
     }
 }
 void main()
 {
-    int input = 3;
-    char arr[7];
+    int input;
+    printf("please enter the input integer:\n");
+    scanf("%d", &input);
+    int size = 2*input +1;
+    char *arr = (char*)malloc(size* sizeof(char));
+    // char arr[7];
     generate(arr, input, input, 0);
+    free(arr);
 }   
