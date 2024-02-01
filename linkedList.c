040bab92c7a7249cf6ae3c62b7731ba1c31e3e04
sidharthn13 <sidharth4network@gmail.com>
2024-02-01 19:17:36 +0530

    created a function to print the linkedList

--- linkedList.c
+++ linkedList.c
@@ -18,9 +18,30 @@ LinkedList createLL(int headValue){
     LinkedList ll = {headNode, headNode};
     return ll;
 }
+void appendToLinkedList(LinkedList *linkedListInstance,int nodeValue){
+    Node *nodeToAppend = (Node*)malloc(sizeof(Node));
+    (*nodeToAppend).value = nodeValue;
+    (*nodeToAppend).next = NULL;
+    (*(*linkedListInstance).current).next = nodeToAppend;
+    (*linkedListInstance).current = nodeToAppend;
+}
+
+void printLinkedList(LinkedList linkedListInstance){                     //linked list instance is passed by value and not by reference
+    Node *curr = linkedListInstance.head;                               //but head and current node markers are passed by pointer values
+    while(curr){
+        printf("%d->",(*curr).value);
+        curr = (*curr).next;
+    }
+    printf("\n");
+}
 
 void main(){
     LinkedList ll1 = createLL(0);
-    printf("the memory address of the head node is : %p\n", (void*)ll1.head);
-    printf("the value of the head node is: %d\n", (*ll1.head).value);
+    appendToLinkedList(&ll1,1);
+    appendToLinkedList(&ll1,2);
+    appendToLinkedList(&ll1,3);
+    appendToLinkedList(&ll1,4);
+    // printf("the memory address of the head node is : %p\n", (void*)ll1.head);
+    // printf("the value of the head node is: %d\n", (*ll1.head).value);
+    printLinkedList(ll1);
 }
