5363fb181a1fc73d430535ae3b01201b80fe7812
sidharthn13 <sidharth4network@gmail.com>
2024-02-10 09:59:43 +0530

    program to convert a given integer into a string

--- test.c
+++ test.c
@@ -1,34 +1,57 @@
+// program to convert a given number into a string
 #include <stdio.h>
 #include <stdlib.h>
-void generate(char arr[], int open, int close, int count)
-{
-
-    if (open == 0 && close == 0)
-    { 
-        *(arr+count) = '\0';
-        printf("%s\n", arr);
-        return;
-    }
-    if (open > 0)
-    { 
-        *(arr+count) = '(';
-        generate(arr, open - 1, close, count + 1);
-    }
-
-    if (open < close)
-    { 
-        *(arr+count) = ')';
-        generate(arr, open, close - 1, count + 1);
-    }
-}
+#include <math.h>
 void main()
 {
-    int input;
-    printf("please enter the input integer:\n");
-    scanf("%d", &input);
-    int size = 2*input +1;
-    char *arr = (char*)malloc(size* sizeof(char));
-    // char arr[7];
-    generate(arr, input, input, 0);
-    free(arr);
-}   
+    int num;
+    printf("enter a number: \n");
+    scanf("%d", &num);
+    int numCpy = num;
+    int numLen = 0;
+    while (numCpy != 0)
+    {
+        numCpy = numCpy / 10;
+        numLen++;
+    }
+    if (num == 0)
+    {
+        printf("the number is: %c\n", '0');
+    }
+    else if (num > 0)
+    {
+        char *result = (char *)malloc((numLen + 1) * sizeof(char));
+        int div = (int)round(pow(10, numLen - 1));
+        result[numLen] = '\0';
+        int index = 0;
+        while (num != 0)
+        {
+            int digit = num / div;
+            char charValue = (char)('0' + digit);
+            result[index] = charValue;
+            num = num % div;
+            div = div / 10;
+            index++;
+        }
+        printf("the  number is : %s\n", result);
+    }
+    else
+    {
+        num = -1 * num;
+        char *result = (char *)malloc((numLen + 2) * sizeof(char));
+        int div = (int)round(pow(10, numLen - 1));
+        result[numLen] = '\0';
+        result[0] = '-';
+        int index = 1;
+        while (num != 0)
+        {
+            int digit = num / div;
+            char charValue = (char)('0' + digit);
+            result[index] = charValue;
+            num = num % div;
+            div = div / 10;
+            index++;
+        }
+        printf("the  number is : %s\n", result);
+    }
+}
